44743d6081be5c831ce14674444fa24b28d9ca88
Derek Verbrugge <52356885+derekverbrugge@users.noreply.github.com>
2022-12-08 02:41:30 -0500

    Update Swap.hs

--- Swap.hs
+++ Swap.hs
@@ -1,4 +1,5 @@
 --Swap of Ada and dollar tokens
+--Use the Haskell editor at https://play.marlowe-finance.io/#/ to compile and run this smart contract example.
 
 {-# LANGUAGE OverloadedStrings #-}
 module Swap where
